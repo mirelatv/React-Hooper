359b3fe414acb7d0178bc05f1e481790f39bf425
mirelatv <noelia.mirela@gmail.com>
2018-03-03 18:20:50 -0500

    subiendo Grace Hooper

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//elemento de  React
+//const hellow = React.createElement("h1" , {id:"no"}, "hola mundo");
+
+//ReactDOM.render( hellow,document.getElementById('react-container'));
+
+
+
+
+//componetes de  React utilizables
+ //const el = <h1> hola mundo</h1>;
+
+
+//  sintaxis  de  script6
+
+ //const User = props => {
+ //return(
+   // <div>
+    //    <img scr={props.img} alt={props.name}/>
+         //<p>{props.nmae}</p>
+  // </div>
+//)
+//}
+//ReactDOM.rende(
+    // ,user img="" name="" 
+ //)
+ ////function Article(props){
+  ////return <h1>{props.title}</h1>
+  //}
+
+//ReactDOM.render(
+//<div>
+  //  <Article title=" Grace Hooper"/>
+//</div>,
+//document.getElementById('root')
+
+//);
+
+// function Cabecera(props) {
+  //  const titulo = 'Saludo';
+  
+   // return (
+     // <div style={{ borderColor: 'red' }}>
+      //  <h2>{titulo}</h2>
+       // <p>
+       //   Hola <b>{props.nombre}</b>.<br>
+       //   Un link? <br />
+         // <a href="http://laboratoria.la">Laboratoria</a>
+       // </p>
+      //</div>
+   // )
+  //}
+  
+ // ReactDOM.render(
+   // <Cabecera nombre="Mundo" />,
+    //document.getElementById('cabecera')
+ // );
+  
